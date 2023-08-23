1c6dd5b4ce742de727c106651db32175295d4d23
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-08-23 21:04:44 +0200

    Index.php created and populated with comments about the assignment

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tal tabel generator</title>
+</head>
+<body>
+    <?php
+    /*
+    Opgave: Løkkebaseret Talgenerator
+
+    Opsummering: Opgaven går ud på at skabe en talgenerator i PHP. I skal bruge variabler, løkker og arrays til at generere en tabel med talrækken i.
+
+    Beskrivelse:
+
+    1. Lav to variabler: en start og en slutværdi
+    2. PHP-scriptet skal generere en række tal (inklusiv start- og slutværdi) ved hjælp af en løkke og gemme disse tal i et array.
+    3. Udskriv de genererede tal i en tabel på websiden.
+
+    
+    Ekstraopgaver:
+
+    1. Sum af tal: Beregn og vis summen af de genererede tal.
+    2. Lige/ulige tæller: Tæl og vis antallet af lige og ulige tal.
+    3. Største og mindste tal: Find og vis det største og mindste tal.
+    4. Find ud af hvordan man lader brugeren indtaste start- og slutværdi via en formular så de ikke er hardcodede
+    */
+    ?>
+</body>
+</html>
